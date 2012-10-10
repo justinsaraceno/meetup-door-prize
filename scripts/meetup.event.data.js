fad8a7509db963d7af8359b740318600a94848f1
justinsaraceno <justin.saraceno@gmail.com>
2012-10-09 23:17:01 -0400

    fixing ref to event url
    this makes it IE10 complient

--- scripts/meetup.event.data.js
+++ scripts/meetup.event.data.js
@@ -10,7 +10,7 @@ $(document).ready(function ($) {
 
     $("#findEvent").click(function (e) {
         e.preventDefault;
-        getEventDetails(eventUrl.value);
+        getEventDetails($("#eventUrl").val());
         return false;
     });
 });
